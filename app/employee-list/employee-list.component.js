945e3848738b8b183a31eeb1d7b9a0738aa16be4
Kevin Fontanilla <kevin.fontanilla@gameloft.com>
2019-05-06 13:03:58 +0800

    fix update employee function

---
+++ app/employee-list/employee-list.component.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('employeeList')
+  .component('employeeList', {
+    templateUrl: 'employee-list/employee-list.template.html',
+    controller: function EmployeeListController($scope) {
+      $scope.employees = [
+        {
+          'id': 1,
+          'lastName': 'Fontanilla',
+          'firstName': 'Kevin',
+          'email': 'kevin.fontanilla50@gmail.com'
+        },
+        {
+          'id': 2,
+          'lastName': 'Salvador',
+          'firstName': 'Janella',
+          'email': 'janella.salvador@gmail.com'
+        },
+        {
+          'id': 3,
+          'lastName': 'Hood',
+          'firstName': 'Robina',
+          'email': 'robina.hood@gmail.com'
+        },
+      ];
+
+      $scope.addEmployee = () => {
+        $scope.employees.unshift($scope.newEmployee)
+      }
+
+      $scope.selectEmployee = (employee) => {
+        $scope.selectedEmployee = employee
+        $scope.selectedEmployeeUpdate = angular.copy(employee)
+      }
+
+      $scope.updateEmployee = (selectedEmployee) => {
+        $scope.employees[selectedEmployee.id - 1].lastName = selectedEmployee.lastName
+        $scope.employees[selectedEmployee.id - 1].firstName = selectedEmployee.firstName
+        $scope.employees[selectedEmployee.id - 1].email = selectedEmployee.email
+      }
+
+      $scope.deleteEmployee = (selectedEmployee) => {
+        var index = $scope.employees.indexOf(selectedEmployee);
+        $scope.employees.splice(index, 1);     
+      }
+    }
+  });
